495a993bd08da0db28b2135f1ab23d71a7b7f529
DESKTOP-2ASGMRL\Nino <nimovic19910813@gmail.com>
2023-06-06 12:26:02 +0800

    张泽亮 第一阶段

---
+++ TestIfElse.java
@@ -0,0 +1,14 @@
+public class TestIfElse {
+	public static void main(String[] args) {
+		int x = 10;
+		if (x == 10) {
+			System.out.println("X的值是10");
+		} else if (x == 20) {
+			System.out.println("X的值是20");
+		} else if (x == 30) {
+			System.out.println("X的值是30");
+		} else {
+			System.out.println("elseyuju");
+		}
+	}
+}
